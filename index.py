918cbf438966315e7f3ad0c9d76faf0f25ad2b5b
trynocs <115831890+Trynocs@users.noreply.github.com>
2024-11-19 21:36:40 +0100

    index.py added.

---
+++ index.py
@@ -0,0 +1,8 @@
+calculation = input("Enter the calculation:\n")
+characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+for i in characters:
+    if i in calculation:
+        print("Error: Invalid calculation")
+        exit()
+solved = eval(calculation)
+print("The answer is: " + str(solved))
